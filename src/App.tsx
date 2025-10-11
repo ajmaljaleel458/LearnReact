86afff5431a0c77e58722ac7f177cfca934b42c5
ajmaljaleel458 <ajmaljaleel.official.458@gmail.com>
2025-10-11 16:13:45 +0530

    feat: enhance Description component with title styling and add user display

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function Description() {
   return (
     <>
-      <h1>Learn React</h1>
+      <h1 className="title">Learn React</h1>
       <p>React is a JavaScript library for building user interfaces.</p>
     </>
   );
@@ -13,10 +13,32 @@ function LikeButton() {
   return <button>Like</button>;
 }
 
+type User = {
+  name: string;
+  imageUrl: string;
+  imageSize: number;
+};
+
+const user: User = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
+
 function App() {
   return (
     <div className="App">
       <Description />
+      <h2>Hey {user.name}</h2>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
       <h1>If you like react then do like it.</h1>
       <LikeButton />
     </div>
