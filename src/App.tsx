bf42587fdce7de8d8fe5a3ca1214646b3e02f7a9
ajmaljaleel458 <ajmaljaleel.official.458@gmail.com>
2025-10-11 16:39:29 +0530

    feat: refactor App component to improve user display and like functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function Description() {
@@ -9,38 +10,74 @@ function Description() {
   );
 }
 
-function LikeButton() {
-  return <button>Like</button>;
+function Greeting({ name, visibility }: { name: string; visibility: boolean }) {
+  return (
+    <h1 style={{ visibility: visibility ? "visible" : "hidden" }}>
+      Thank you {name} for liking react
+    </h1>
+  );
+}
+
+function LikeButton({ onLiked }: { onLiked: () => void }) {
+  return <button onClick={onLiked}>Like</button>;
 }
 
 type User = {
+  id: number;
   name: string;
-  imageUrl: string;
-  imageSize: number;
+  avatar?: string;
 };
 
-const user: User = {
-  name: "Hedy Lamarr",
-  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
-  imageSize: 90,
+const user = {
+  id: 0,
+  name: "John Doe",
+  avatar: "https://i.pravatar.cc/40?u=JohnDoe",
 };
+
+const users: User[] = [
+  { id: 1, name: "Alice Johnson", avatar: "https://i.pravatar.cc/40?u=alice" },
+  { id: 2, name: "Bob Smith", avatar: "https://i.pravatar.cc/40?u=bob" },
+  {
+    id: 3,
+    name: "Charlie Brown",
+    avatar: "https://i.pravatar.cc/40?u=charlie",
+  },
+];
+
+function LikedUsers({ users }: { users: User[] }) {
+  const likedUsers = users.map((user) => (
+    <li key={user.id}>
+      {user.name} <img src={user.avatar} alt="user image" />
+    </li>
+  ));
+  return <ul>{likedUsers}</ul>;
+}
 
 function App() {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setIsLiked(true);
+  };
+
   return (
     <div className="App">
       <Description />
       <h2>Hey {user.name}</h2>
       <img
         className="avatar"
-        src={user.imageUrl}
+        src={user.avatar}
         alt={"Photo of " + user.name}
         style={{
-          width: user.imageSize,
-          height: user.imageSize,
+          width: user.avatar,
+          height: user.avatar,
         }}
       />
       <h1>If you like react then do like it.</h1>
-      <LikeButton />
+      <LikeButton onLiked={handleLike} />
+      <Greeting visibility={isLiked} name={user.name} />
+      <h2>Other users liked</h2>
+      <LikedUsers users={users} />
     </div>
   );
 }
